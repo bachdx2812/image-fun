294c2597ff04b7c16976b10bcdbb6bb9a6f1ebfb
bachdx <bachdx.hut@gmail.com>
2018-09-17 03:16:14 +0900

    init

---
+++ src/components/Gallery.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+  <div id='gallery'>
+    <div class="masonry masonry--h">
+      <figure v-for="image in images" class="masonry-brick masonry-brick--h">
+        <img :src="image.urls.regular" class="masonry-img" @click="openImageEditor(image)">
+      </figure>
+    </div>
+  </div>
+  <ImageEditor v-if="currentImage" :image="currentImage"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import ImageEditor from './ImageEditor.vue';
+
+const API_END_POINT = 'https://api.unsplash.com';
+const API_ACCESS_KEY = '027fbdbaa9b670f0009962019861081abff81fec413a30c5f90aa7c591343ef5';
+
+export default {
+  components: {
+    ImageEditor
+  },
+  props: {
+  },
+  data: function() {
+    return {
+      images: [],
+      currentImage: null
+    }
+  },
+  created: function(){
+    this.fetchImages();
+  },
+  methods: {
+    fetchImages: function(){
+      let self = this;
+
+      axios.get(`${API_END_POINT}/photos?client_id=${API_ACCESS_KEY}&per_page=50`)
+        .then(function(response){
+          self.images = response.data;
+        })
+    },
+    openImageEditor: function(image) {
+      this.currentImage = image;
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.masonry {
+  display: flex;
+  width: 100%;
+}
+
+.masonry--h {
+  flex-flow: row wrap;
+}
+
+.masonry--v {
+  flex-flow: column wrap;
+  max-height: 1080px;
+}
+
+.masonry--h, .masonry--v {
+  margin-left: -8px; /* Adjustment for the gutter */
+  counter-reset: brick;
+}
+
+.masonry-brick {
+  overflow: hidden;
+  border-radius: 5px;
+  margin: 0 0 8px 8px;  /* Some Gutter */
+  background-color: #333;
+  color: white;
+  position: relative;
+}
+
+.masonry-brick:hover:after {
+  font-size: 2.25em;
+  opacity: 1;
+}
+
+.masonry-brick--h {
+  flex: auto;
+  height: 250px;
+  min-width: 150px;
+}
+
+@media only screen and (min-width: 1024px) {
+  /* Horizontal masonry bricks on desktop-sized screen */
+  .masonry-brick--h:nth-child(4n+1) {
+    width: 250px;
+  }
+  .masonry-brick--h:nth-child(4n+2) {
+    width: 325px;
+  }
+  .masonry-brick--h:nth-child(4n+3) {
+    width: 180px;
+  }
+  .masonry-brick--h:nth-child(4n+4) {
+    width: 380px;
+  }
+
+  /* Adjusting vertical masonry height on desktop-sized screen */
+  .masonry--v {
+    max-height: 1600px;
+  }
+
+  /* Vertical masonry bricks on desktop-sized screen */
+  .masonry-brick--v {
+    width: 33.33333%;
+  }
+}
+
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  /* Horizontal masonry bricks on tabled-sized screen */
+  .masonry-brick--h:nth-child(4n+1) {
+    width: 200px;
+  }
+  .masonry-brick--h:nth-child(4n+2) {
+    width: 250px;
+  }
+  .masonry-brick--h:nth-child(4n+3) {
+    width: 120px;
+  }
+  .masonry-brick--h:nth-child(4n+4) {
+    width: 280px;
+  }
+
+  /* Adjusting vertical masonry height on tablet-sized screen */
+  .masonry--v {
+    max-height: 2000px;
+  }
+
+  /* Vertical masonry bricks on tablet-sized screen */
+  .masonry-brick--v {
+    width: 50%;
+  }
+}
+
+.masonry-img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ src/components/ImageEditor.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <div class="grid-cell fixed">
+        <figure v-bind:class="customStyle">
+          <img :src="image.urls.regular">
+        </figure>
+      </div>
+      <div class="grid-cell filter-list">
+        <div class="filter-item" v-for="style in styles" @click="filterImage(style)">
+          <figure :class="style.name">
+            <img :src="image.urls.thumb">
+          </figure>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  props: {
+    image: {
+      type: Object,
+      required: true
+    }
+  },
+  data: function(){
+    return {
+      customStyle: "normal"
+    }
+  },
+  created: function(){
+    this.styles = [
+      { name: "normal" },
+      { name: "clarendon" },
+      { name: "gingham" },
+      { name: "moon" },
+      { name: "lark" },
+      { name: "reyes" },
+      { name: "juno" },
+      { name: "slumber" },
+      { name: "aden" },
+      { name: "perpetua" },
+      { name: "mayfair" },
+      { name: "rise" },
+      { name: "hudson" },
+      { name: "valencia" },
+      { name: "xpro2" },
+      { name: "willow" },
+      { name: "lofi" },
+      { name: "inkwell" },
+      { name: "nashville" }
+    ]
+  },
+  mounted: function(){
+  },
+  methods: {
+    filterImage: function(style){
+      this.customStyle = style.name;
+    }
+  }
+}
+</script>
+
+<style>
+
+.modal {
+    display: block; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto; /* 15% from the top and centered */
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%; /* Could be more or less, depending on screen size */
+    display: flex;
+}
+
+/* The Close Button */
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.fixed {
+  width: 80%;
+}
+
+.filter-list {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow: scroll;
+  max-height: 1000px;
+}
+
+.grid-cell {
+  margin: 5px;
+  padding: 0px;
+  line-height: 1;
+}
+
+img {
+  width: 100%;
+  height: auto;
+}
+
+.filter-item {
+  cursor: pointer;
+  margin: 1px;
+}
+</style>
